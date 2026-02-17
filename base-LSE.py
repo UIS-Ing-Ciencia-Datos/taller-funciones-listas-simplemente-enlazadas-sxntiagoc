0888c8b94975c40add894350493f72bc7f7dc679
sxntiagoc <santiagcortezz@gmail.com>
2026-02-17 18:28:29 -0500

    Add files via upload

--- base-LSE.py
+++ base-LSE.py
@@ -1,32 +1,64 @@
-# ===========================================
-# By: Nury Farelo - Estructuras Datos
-# Name: Lista Simplemente Enlazada 
-# ===========================================
+# Agregar al final
 
-# Clase Nodo
-class Nodo:
-	def __init__(self, data):
-		self.data = data
-		self.siguiente = None
+def agregarFinal(self, data):
+    nuevo_nodo = Nodo(data)
 
-# CLase Listas enlazada simple
-class ListaSE:
-	def __init__(self):
-		self.cabeza = None
-  
-  	# Lista Vacia
-	def vacio(self):
-		if self.cabeza == None:
-			print("Está vacia")
-		else:
-			print("Lista no vacia")
+    # Caso 1: lista vacia
+    if self.cabeza is None:
+        self.cabeza = nuevo_nodo
+        return
 
-	# Agregar al inicio
-	def agregarInicio(self, data):
-		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
-			return
-		else:
-			nuevo_nodo.siguiente = self.cabeza
-			self.cabeza = nuevo_nodo
+    # Caso 2: recorrer hasta el final
+    actual = self.cabeza
+    while actual.siguiente:
+        actual = actual.siguiente
+
+    actual.siguiente = nuevo_nodo
+
+# Insertar despues de un elemento X
+
+def insertarDespues(self, valor_buscar, data):
+    actual = self.cabeza
+
+    while actual:
+        if actual.data == valor_buscar:
+            nuevo_nodo = Nodo(data)
+            nuevo_nodo.siguiente = actual.siguiente
+            actual.siguiente = nuevo_nodo
+            return
+        actual = actual.siguiente
+
+    print("Elemento no encontrado")
+
+# Insertar antes de un elemento X
+
+def insertarAntes(self, valor_buscar, data):
+    if self.cabeza is None:
+        print("Lista vacia")
+        return
+
+    # Caso 1: antes del primero
+    if self.cabeza.data == valor_buscar:
+        self.agregarInicio(data)
+        return
+
+    actual = self.cabeza
+
+    while actual.siguiente:
+        if actual.siguiente.data == valor_buscar:
+            nuevo_nodo = Nodo(data)
+            nuevo_nodo.siguiente = actual.siguiente
+            actual.siguiente = nuevo_nodo
+            return
+        actual = actual.siguiente
+
+    print("Elemento no encontrado")
+
+#  Eliminar el primero
+
+def eliminarPrimero(self):
+    if self.cabeza is None:
+        print("Lista vacía")
+        return
+
+    self.cabeza = self.cabeza.siguiente
